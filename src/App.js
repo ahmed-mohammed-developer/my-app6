f6bfeec81daaf93b09d6562a4d5c61417618b144
ahmed <ahmed.zaakan@gmail.com>
2024-10-01 09:16:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+const showH1 = false;
+const data = [
+  {id: 1, name: "A"},
+  {id: 5, name: "A"},
+  {id: 12, name: "A"},
+]
+const datamap = data.map((data1) => {
+  return (<li key={data1.id}>{data1.name}</li>)
+})
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {showH1 === true ? <h1>Ahmed Mo</h1> : <h2>Zakan</h2>}
+        <div >
+          <ul>
+            {datamap}
+          </ul>
+        </div>
       </header>
     </div>
   );
